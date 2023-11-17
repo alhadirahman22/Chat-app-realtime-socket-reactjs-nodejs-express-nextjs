eeab5a1422b5482af6b7da271b5e7cf0b7a59ed0
Alhadi Rahman <alhadirahman22@gmail.com>
2023-11-17 21:09:00 +0700

    feat: :sparkles: Fix styling

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,15 @@
 const Layout = ({ children }) => {
     return (
         <div className="mx-auto bg-white">
-            <div className="bg-main relative">
+            {/* <div className="bg-main relative"> */}
+            <div className="bg-main">
                 <main className="font-sans mt-2 mb-4">
                     <div className="mx-auto w-full max-w-md mb-4">
                         <div className="mx-2">
                             <div className="p-3 bg-white/90">
                                 {children}
                             </div>
+
                         </div>
                     </div>
                 </main>
--- pages/[pid] - Copy.js
+++ pages/[pid] - Copy.js
@@ -2,9 +2,11 @@ import { useRouter } from 'next/router';
 const Chatting = () => {
     const router = useRouter();
     const { pid } = router.query;
+
     return (
         <>
-            <div className='mx-auto w-full   px-3 relative' style={{ height: "100%" }}>
+            {/* <div className='mx-auto w-full border shadow px-3 relative overflow-y-auto' style={{ maxHeight: "600px" }}> */}
+            <div className='mx-auto w-full border shadow px-3 relative' style={{ height: "100%" }}>
                 <div className="w-full flex" style={{
                     backgroundColor: "white",
                     top: 0,
@@ -52,34 +54,19 @@ const Chatting = () => {
                         <p>Your message here aaaaaasads  asdsadsa  asdsad  asdasd</p>
                     </div>
                 </div>
-                {/* <button className=" p-1 rounded-md md:bottom-3 md:p-2 md:right-3 dark:hover:bg-gray-900 dark:disabled:hover:bg-transparent right-2 gizmo:dark:disabled:bg-white gizmo:disabled:bg-black gizmo:disabled:opacity-10 disabled:text-gray-400 enabled:bg-brand-purple gizmo:enabled:bg-black text-white gizmo:p-0.5 gizmo:border gizmo:border-black gizmo:rounded-lg gizmo:dark:border-white gizmo:dark:bg-white bottom-1.5 transition-colors disabled:opacity-40" data-testid="send-button"><span className="" data-state="closed">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" className="rounded-full bg-green-500 text-white dark:text-black"><path d="M7 11L12 6L17 11M12 18V7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg></span></button> */}
-
-
-                <div className="w-full flex" style={{
-                    backgroundColor: "white",
-                    bottom: 0,
-                    left: "0px",
-                    position: "sticky",
-                    zIndex: "1",
-                    padding: "10px",
-
-                }} >
-                    <input
-                        type="text"
-                        placeholder='RoomID'
-                        style={{
-                            backgroundColor: "#F6F6F6",
-
-                        }}
-                        className={`border rounded-full w-full mb-4 mt-10 p-4`}
-                    />
-                    <svg width="36" height="36" viewBox="0 0 24 24" fill="none" className="rounded-full bg-green-500 text-white dark:text-black" style={{
-                        position: "absolute",
-                        top: "61.5px",
-                        right: "25px"
-                    }} ><path d="M7 11L12 6L17 11M12 18V7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>
-                </div>
+                <input
+                    type="text"
+                    placeholder='RoomID'
+                    style={{
+                        backgroundColor: "#F6F6F6",
+                        // bottom: 0,
+                        left: "0px",
+                        position: "sticky",
+                        zIndex: "1",
+                        // padding: "10px",
+                    }}
+                    className={`border rounded-full w-full mb-4 mt-10 p-4 bottom-0`}
+                />
             </div>
         </>
     )
